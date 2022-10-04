c4b39754cc386b9bb570ae6da5a8b3d22e905419
Nitesh-54 <nitesh54007@gmail.com>
2022-10-05 02:29:17 +0530

    End of SQL Audit

--- SQL_Auditing.ps1
+++ SQL_Auditing.ps1
@@ -87,15 +87,15 @@ function SQL4_1_5 {
         foreach ($name in $SQLServerList) {
             $SQLServerDatabasesList = Get-AzSqlDatabase -ServerName $name.Servername -ResourceGroupName $name.ResourceGroupName
             foreach ($name in $SQLServerDatabasesList) {
-                $SQLServerTDE = Get-AzSqlDatabaseTransparentDataEncryption  -ServerName $name.Servername -ResourceGroupName $name.ResourceGroupName -DatabaseName $name.DatabaseName
-                if ($SQLServerTDE.DatabaseName -ne 'master' -and $SQLServerTDE.State -ne 'Enabled') {
-                    $effectedResources += $SQLServerTDE.DatabaseName
+                $SQLServerTDEstate = Get-AzSqlDatabaseTransparentDataEncryption  -ServerName $name.Servername -ResourceGroupName $name.ResourceGroupName -DatabaseName $name.DatabaseName
+                if ($SQLServerTDEstate.DatabaseName -ne 'master' -and $SQLServerTDEstate.State -ne 'Enabled') {
+                    $effectedResources += $SQLServerTDEstate.DatabaseName
                 }
             }
         }
         $desc = "Enable Transparent Data Encryption on every SQL server."
         $rem = "Ensure that 'Data encryption' is set to 'On' on a SQL Database"
-        AddToExportArray -type "SQLServerDatabasesList" -cisid "4.1.5" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+        AddToExportArray -type "SQLServerTDEstate" -cisid "4.1.5" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
     }
     catch {
         Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
@@ -123,7 +123,7 @@ function SQL4_1_6 {
         }
         $desc = "SQL Server Audit Retention should be configured to be greater than 90 days."
         $rem = "Ensure that 'Auditing' Retention is 'greater than 90 days'"
-        AddToExportArray -type "SQLServerAuditingSettings" -cisid "4.1.6" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+        AddToExportArray -type "SQLServerWorkspaceInfo" -cisid "4.1.6" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
     }
     catch {
         Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
@@ -136,16 +136,16 @@ function SQL4_2_1 {
         $effectedResources = @()
         $SQLServerList = Get-AzSqlServer
         foreach ($name in $SQLServerList) {
-            $SQLServerThreatProtection = Get-AzSqlServerAdvancedThreatProtectionSetting -ResourceGroupName $name.ResourceGroupName -ServerName $name.Servername
-            foreach ($name in $SQLServerThreatProtection) {
-                if ($SQLServerThreatProtection.ThreatDetectionState -ne "enabled") {
-                    $effectedResources += $SQLServerThreatProtection.ServerName
+            $SQLServerThreatProtectionSettings = Get-AzSqlServerAdvancedThreatProtectionSetting -ResourceGroupName $name.ResourceGroupName -ServerName $name.Servername
+            foreach ($name in $SQLServerThreatProtectionSettings) {
+                if ($SQLServerThreatProtectionSettings.ThreatDetectionState -ne "enabled") {
+                    $effectedResources += $SQLServerThreatProtectionSettings.ServerName
                 }
             }
         }
         $desc = "Enable 'Microsoft Defender for SQL' on critical SQL Servers."
         $rem = "Ensure that Microsoft Defender for SQL is set to 'On' for critical SQL Servers"
-        AddToExportArray -type "SQLServerThreatProtection" -cisid "4.2.1" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+        AddToExportArray -type "SQLServerThreatProtectionSettings" -cisid "4.2.1" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
     }
     catch {
         Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
@@ -160,7 +160,7 @@ function SQL4_2_2 {
         foreach ($name in $SQLServerList) {
             $SQLServerVulnerabilityAssessmentSettings = Get-AzSqlServerVulnerabilityAssessmentSetting -ResourceGroupName $name.ResourceGroupName -ServerName $name.Servername
             if ($null -eq $SQLServerVulnerabilityAssessmentSettings.StorageAccountName) {
-                $effectedResources += $SQLServerThreatProtection.ServerName
+                $effectedResources += $SQLServerVulnerabilityAssessmentSettings.ServerName
             }
         }
         $desc = "Enable Vulnerability Assessment (VA) service scans for critical SQL servers and corresponding SQL databases."
@@ -181,7 +181,7 @@ function SQL4_2_3 {
         foreach ($name in $SQLServerList) {
             $SQLServerVulnerabilityAssessmentSettings = Get-AzSqlServerVulnerabilityAssessmentSetting -ResourceGroupName $name.ResourceGroupName -ServerName $name.Servername
             if ($SQLServerVulnerabilityAssessmentSettings.RecurringScansInterval -eq 'None') {
-                $effectedResources += $SQLServerThreatProtection.ServerName
+                $effectedResources += $SQLServerVulnerabilityAssessmentSettings.ServerName
             }
         }
         $desc = "Enable Vulnerability Assessment (VA) Periodic recurring scans for critical SQL servers and corresponding SQL databases."
@@ -201,7 +201,7 @@ function SQL4_2_4 {
         foreach ($name in $SQLServerList) {
             $SQLServerVulnerabilityAssessmentSettings = Get-AzSqlServerVulnerabilityAssessmentSetting -ResourceGroupName $name.ResourceGroupName -ServerName $name.Servername
             if ($null -eq $SQLServerVulnerabilityAssessmentSettings.NotificationEmail) {
-                $effectedResources += $SQLServerThreatProtection.ServerName
+                $effectedResources += $SQLServerVulnerabilityAssessmentSettings.ServerName
             }
         }
         $desc = "Configure 'Send scan reports to' with email ids of concerned data owners/stakeholders for a critical SQL servers."
@@ -221,7 +221,7 @@ function SQL4_2_5 {
         foreach ($name in $SQLServerList) {
             $SQLServerVulnerabilityAssessmentSettings = Get-AzSqlServerVulnerabilityAssessmentSetting -ResourceGroupName $name.ResourceGroupName -ServerName $name.Servername
             if ($SQLServerVulnerabilityAssessmentSettings.EmailAdmins -ne 'true') {
-                $effectedResources += $SQLServerThreatProtection.ServerName
+                $effectedResources += $SQLServerVulnerabilityAssessmentSettings.ServerName
             }
         }
         $desc = "Enable Vulnerability Assessment (VA) setting 'Also send email notifications to admins and subscription owners'."
@@ -253,18 +253,133 @@ function SQL_4_3_1 {
     }
 }
 
+function SQL_4_3_2 {
+    try {
+        $effectedResources = @()
+        $RG = Get-AzResourceGroup
+        foreach ($names in $RG) {
+            $PostgreSQLServerInfo = Get-AzPostgreSqlServer -ResourceGroupName $names.ResourceGroupName
+            foreach ($name in $PostgreSQLServerInfo) {
+                $PostgreSQLConfigurationInfo = Get-AzPostgreSqlConfiguration -Name log_checkpoints -ServerName $name.Name -ResourceGroupName $names.ResourceGroupName
+                if ($PostgreSQLConfigurationInfo.Value -ne 'on' ) {
+                    $effectedResources += $name.Name
+                }
+            }
+        }
+        $desc = "Enable log_checkpoints on PostgreSQL Servers."
+        $rem = "Ensure Server Parameter 'log_checkpoints' is set to 'ON' for PostgreSQL Database Server"
+        AddToExportArray -type "PostgreSQLConfigurationInfo" -cisid "4.3.2" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+    }
+    catch {
+        Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
+        Write-Host $_
+    }
+}
+
+function SQL_4_3_3 {
+    try {
+        $effectedResources = @()
+        $RG = Get-AzResourceGroup
+        foreach ($names in $RG) {
+            $PostgreSQLServerInfo = Get-AzPostgreSqlServer -ResourceGroupName $names.ResourceGroupName
+            foreach ($name in $PostgreSQLServerInfo) {
+                $PostgreSQLConfigurationInfo = Get-AzPostgreSqlConfiguration -Name log_connections -ServerName $name.Name -ResourceGroupName $names.ResourceGroupName
+                if ($PostgreSQLConfigurationInfo.Value -ne 'on' ) {
+                    $effectedResources += $name.Name
+                }
+            }
+        }
+        $desc = "Enable log_connections on PostgreSQL Servers."
+        $rem = "Ensure server parameter 'log_connections' is set to 'ON' for PostgreSQL Database Server"
+        AddToExportArray -type "PostgreSQLConfigurationInfo" -cisid "4.3.3" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+    }
+    catch {
+        Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
+        Write-Host $_
+    }
+}
+
+function SQL_4_3_4 {
+    try {
+        $effectedResources = @()
+        $RG = Get-AzResourceGroup
+        foreach ($names in $RG) {
+            $PostgreSQLServerInfo = Get-AzPostgreSqlServer -ResourceGroupName $names.ResourceGroupName
+            foreach ($name in $PostgreSQLServerInfo) {
+                $PostgreSQLConfigurationInfo = Get-AzPostgreSqlConfiguration -Name log_disconnections -ServerName $name.Name -ResourceGroupName $names.ResourceGroupName
+                if ($PostgreSQLConfigurationInfo.Value -ne 'on' ) {
+                    $effectedResources += $name.Name
+                }
+            }
+        }
+        $desc = "Enable log_disconnections on PostgreSQL Servers."
+        $rem = "Ensure server parameter 'log_disconnections' is set to 'ON' for PostgreSQL Database Server"
+        AddToExportArray -type "PostgreSQLConfigurationInfo" -cisid "4.3.4" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+    }
+    catch {
+        Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
+        Write-Host $_
+    }
+}
+
+function SQL_4_3_5 {
+    try {
+        $effectedResources = @()
+        $RG = Get-AzResourceGroup
+        foreach ($names in $RG) {
+            $PostgreSQLServerInfo = Get-AzPostgreSqlServer -ResourceGroupName $names.ResourceGroupName
+            foreach ($name in $PostgreSQLServerInfo) {
+                $PostgreSQLConfigurationInfo = Get-AzPostgreSqlConfiguration -Name connection_throttling -ServerName $name.Name -ResourceGroupName $names.ResourceGroupName
+                if ($PostgreSQLConfigurationInfo.Value -ne 'on' ) {
+                    $effectedResources += $name.Name
+                }
+            }
+        }
+        $desc = "Enable connection_throttling on PostgreSQL Servers."
+        $rem = "Ensure server parameter 'connection_throttling' is set to 'ON' for PostgreSQL Database Server"
+        AddToExportArray -type "PostgreSQLConfigurationInfo" -cisid "4.3.5" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+    }
+    catch {
+        Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
+        Write-Host $_
+    }
+}
+
+function SQL_4_3_6 {
+    try {
+        $effectedResources = @()
+        $RG = Get-AzResourceGroup
+        foreach ($names in $RG) {
+            $PostgreSQLServerInfo = Get-AzPostgreSqlServer -ResourceGroupName $names.ResourceGroupName
+            foreach ($name in $PostgreSQLServerInfo) {
+                $PostgreSQLConfigurationInfo = Get-AzPostgreSqlConfiguration -Name log_retention_days -ServerName $name.Name -ResourceGroupName $names.ResourceGroupName
+                if ($PostgreSQLConfigurationInfo.Value -le 3 ) {
+                    $effectedResources += $name.Name
+                }
+            }
+        }
+        $desc = "Enable log_retention_days on PostgreSQL Servers."
+        $rem = "Ensure Server Parameter 'log_retention_days' is greater than 3 days for PostgreSQL Database Server"
+        AddToExportArray -type "PostgreSQLConfigurationInfo" -cisid "4.3.6" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+    }
+    catch {
+        Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
+        Write-Host $_
+    }
+}
+
 function SQL_4_3_8 {
     try {
         $effectedResources = @()
-        $PostGreSQLServerInfo = Get-AzPostgreSqlServer
-        foreach ($name in $PostGreSQLServerInfo) {
-            if($name.InfrastructureEncryption -ne 'Enabled') {
+        $PostgreSQLServerInfo = Get-AzPostgreSqlServer
+        foreach ($name in $PostgreSQLServerInfo) {
+            if ($name.InfrastructureEncryption -ne 'Enabled') {
                 $effectedResources += $name.Name
             }
         }
         $desc = "Enable encryption at rest for PostgreSQL Databases."
         $rem = "Ensure 'Infrastructure double encryption' for PostgreSQL Database Server is 'Enabled'"
-        AddToExportArray -type "PostGreSQLServerInfo" -cisid "4.3.8" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+        AddToExportArray -type "PostgreSQLServerInfo" -cisid "4.3.8" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
     }
     catch {
         Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
@@ -275,15 +390,38 @@ function SQL_4_3_8 {
 function SQL_4_4_1 {
     try {
         $effectedResources = @()
-            $SQLServerInformation = Get-AzMySqlServer
-            foreach ($name in $SQLServerInformation) {
+        $MySQLServerInformation = Get-AzMySqlServer
+        foreach ($name in $MySQLServerInformation) {
             if ($name.SslEnforcement -ne 'Enabled') {
                 $effectedResources += $name.Name
             }
         }
         $desc = "Enable SSL connection on MYSQL Servers."
         $rem = "Ensure 'Enforce SSL connection' is set to 'Enabled' for Standard MySQL Database Server"
-        AddToExportArray -type "SQLServerInformation" -cisid "4.4.1" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+        AddToExportArray -type "MySQLServerInformation" -cisid "4.4.1" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
+    }
+    catch {
+        Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
+        Write-Host $_
+    }
+}
+
+function SQL_4_4_2 {
+    try {
+        $effectedResources = @()
+        $RG = Get-AzResourceGroup
+        foreach ($names in $RG) {
+            $MySQLflexibleServerInfo = Get-AzMySqlFlexibleServer -ResourceGroupName $names.ResourceGroupName
+            foreach ($name in $MySQLflexibleServerInfo) {
+                $SQLConfig = Get-AzMySqlFlexibleServerConfiguration -Name tls_version -ServerName $name.Name -ResourceGroupName $names.ResourceGroupName
+                if ($SQLConfig.Value -ne 'TLSV1.2' ) {
+                    $effectedResources += $name.Name
+                }
+            }
+        }
+        $desc = "Ensure TLS version on MySQL flexible servers is set to the default value."
+        $rem = "Ensure 'TLS Version' is set to 'TLSV1.2' for MySQL flexible Database Server"
+        AddToExportArray -type "MySQLflexibleServerInfo" -cisid "4.4.2" -description $desc -subSeverity "Medium" -status (StatusCheck -inputArray $effectedResources) -remidiation $rem -effectedResource $effectedResources
     }
     catch {
         Write-Host (Get-Date -UFormat '%y_%m_%d') '- The following error occured: '
@@ -340,13 +478,13 @@ try {
     #SQLServer_AD_Admin_info
     SQL4_1_4 > $null
 
-    #SQLServerDatabasesList
+    #SQLServerTDEstate
     SQL4_1_5 > $null
 
-    #SQLServerAuditingSettings
+    #SQLServerWorkspaceInfo
     SQL4_1_6 > $null
 
-    #SQLServerThreatProtection
+    #SQLServerThreatProtectionSettings
     SQL4_2_1 > $null
 
     #SQLServerVulnerabilityAssessmentSettings
@@ -355,12 +493,25 @@ try {
     SQL4_2_4 > $null
     SQL4_2_5 > $null
 
-    #PostGreSQLServerInfo
+    #PostgreSQLConfigurationInfo
     SQL_4_3_1 > $null
+
+    #PostgreSQLConfigurationInfo
+    SQL_4_3_2 > $null
+    SQL_4_3_3 > $null
+    SQL_4_3_4 > $null
+    SQL_4_3_5 > $null
+    SQL_4_3_6 > $null
+
+
+    #PostgreSQLServerInfo
     SQL_4_3_8 > $null
 
-    #SQLServerInformation
+    #MySQLServerInformation
     SQL_4_4_1 > $null
+
+    #MySQLflexibleServerInfo
+    SQL_4_4_2 > $null
 
     $global:exportArray
 }
